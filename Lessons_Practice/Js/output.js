840cc74b9aec07af813aca6254c6e3a33839ccdb
adhamkhwaldeh <31662377+adhamkhwaldeh@users.noreply.github.com>
2018-04-21 12:23:53 +0300

    Add files via upload

---
+++ Lessons_Practice/Js/output.js
@@ -0,0 +1,8 @@
+
+function clickfunction(){
+    document.getElementById('mainParagraph').innerText="The Text Changed";
+}
+
+function clickfunction2(){
+    document.getElementById('mainParagraph').innerText="This is initial state for paragraph";
+}
---
+++ Lessons_Practice/Js/statement.js
@@ -0,0 +1,10 @@
+//statement started
+var x, y, z;    // Statement 1
+x = 5;          // Statement 2
+y = 6;          // Statement 3
+z = x + y;      // Statement 4
+
+//operation started (= + - * /)
+
+
+//statement ended
